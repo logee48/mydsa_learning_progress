8f14b83755bddb0cc6ab45e0a43d1a011bf5baa3
logeshwaran Elumalai <81499495+logee48@users.noreply.github.com>
2022-09-08 10:57:21 +0530

    3

--- leetcode_problems.py
+++ leetcode_problems.py
@@ -20,4 +20,62 @@ class Solution:
         l1 = l1.next if l1 else None
         l2 = l2.next if l2 else None
       return dummy.next             # dummy first val is None, so values are stored from next to head node
+
+
+  
+# 13 Roman to integer   08/09/2022
+# mistake i did, didn't read the question properly
+# MCMXCIV in 2nd iteration CM will be compared and C<M is true, so we need to add M-C to sum and skip M in next part and go to XC
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        sum_val = 0
+        r_val = {'I':1, 'V':5, 'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000};      #using dist to store roman values
+        length = 0                 #using while because after compare statement passed we need to ""skip one iteration part""
+        if(len(s)>1):
+            while(length<len(s)-1):
+                if(r_val[s[length]] < r_val[s[length+1]]):
+                    sum_val += r_val[s[length+1]] - r_val[s[length]]
+                    length+=2
+                else:
+                    sum_val += r_val[s[length]]
+                    length+=1
+            if(r_val[s[-2]] >= r_val[s[-1]]):               #if last ele is less than 2nd to last, add last ele to sum_val
+                sum_val+=r_val[s[-1]]
+        else:                                     #for single char test case
+            sum_val+= r_val[s]
+
+        return sum_val
     
+    
+# 844 Backspace string compare   08/09/2022
+# i didn't solve it on my own, i saw solution is diss part
+# using stack this problem can be solved
+# understood where to use stack
+"""testcase
+s = "ac#d"
+t = "ace##d"
+if u get #, then remove element before #
+s = ad
+t = ad
+output: true, hence s==t
+"""
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        one = []
+        two = []
+        for i in range(len(s)):
+            if (s[i] != "#"):
+                one.append(s[i])
+            else:
+                if(one!=[]):
+                    one.pop(-1)
+        for i in range(len(t)):
+            if (t[i]!= "#"):
+                two.append(t[i])
+            else:
+                if(two!=[]):
+                    two.pop(-1)
+        return one==two
